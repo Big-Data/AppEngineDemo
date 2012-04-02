bc323cee026feec375b645daac3a6e8faa831260
Bruno Morency <bruno@context.io>
2012-04-02 11:28:11 -0400

    Added stub settings.py file

---
+++ settings.py
@@ -0,0 +1,30 @@
+# Your Context.IO Consumer key and secret used to sign calls to
+# https://api.context.io.
+#
+# To get those keys, create a free account through
+# http://context.io/#signup.
+#
+# If you already have an account, you can retrieve you consumer
+# key and secret in the settings tab of the console:
+# https://console.context.io/#settings
+
+CONTEXTIO_OAUTH_KEY = 'YOUR-CONTEXTIO-CONSUMER-KEY'
+CONTEXTIO_OAUTH_SECRET = 'YOUR-CONTEXTIO-CONSUMER-SECRET'
+CONTEXTIO_API_URL = 'https://api.context.io'
+
+# Your Google Data API consumer key and secret. These are used
+# to obtain access tokens giving the app access to Gmail accounts.
+# follow these instructions to obtain them:
+# http://code.google.com/apis/accounts/docs/RegistrationForWebAppsAuto.html
+#
+# Make sure this consumer key and secret are also configured in
+# your Context.IO account. You can do this in the settings tab
+# of the Context.IO console: 
+# https://console.context.io/#settings
+
+APPENGINE_CONSUMER_KEY = 'GOOGLE-OAUTH-CONSUMER-KEY'
+APPENGINE_CONSUMER_SECRET = 'GOOGLE-OAUTH-CONSUMER-SECRET'
+
+# The name of you App Engine application
+
+APPENGINE_APP_NAME = 'contextio-demo'
